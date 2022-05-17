53501343c64bd10f1353944e111f9be5c056e11b
Dome2066 <86346712+Dome2066@users.noreply.github.com>
2022-05-17 16:10:38 +0200

    Add files via upload

---
+++ inventur_backend/src/main/java/de/htw/inventur/config/CorsConfiguration.java
@@ -0,0 +1,27 @@
+package de.htw.inventur.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class CorsConfiguration implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedMethods("GET", "POST", "PUT", "DELETE");
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("http://localhost:8080", "http://localhost:3000");
+            }
+        };
+    }
+}
