5ec106ad40ac6802576fcb005bc6bc777e156718
Dome2066 <s82063@htw-dresden.de>
2022-06-02 12:35:00 +0200

    Object entity um roomName, sectionName, storageName und lendBy erweitert - somit auch die Datenbank

--- inventur_backend/src/main/java/de/htw/inventur/repository/ObjectRepository.java
+++ inventur_backend/src/main/java/de/htw/inventur/repository/ObjectRepository.java
@@ -11,6 +11,8 @@ import java.util.List;
 @Repository
 public interface ObjectRepository extends JpaRepository<Object, Integer> {
 
+    Object findById(int id);
+
     List<Object> findAllBySectionId(int sectionId);
 
     List<Object> findAll();
