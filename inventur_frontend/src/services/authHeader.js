1e060ab3a26311114af4ba56fa987b4c0ee01b06
Dome2066 <s82063@htw-dresden.de>
2022-06-24 13:10:07 +0200

    CORS Problemlösungsversuch 2

--- inventur_frontend/src/services/authHeader.js
+++ inventur_frontend/src/services/authHeader.js
@@ -1,7 +1,7 @@
 export default function authHeader(){
     const user = JSON.parse(localStorage.getItem('user'));
     if(user){
-        return {Authorization: 'Bearer ' + user};
+        return {Authorization: 'Bearer ' + user, 'Access-Control-Allow-Origin': '*'};
     } else {
         return {'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*'};
     }
